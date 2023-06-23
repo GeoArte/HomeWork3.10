aa042028695b72b5cec8b6b460067dc85a9233b2
gosha <gosha.artemov@gmail.com>
2023-06-24 00:47:53 +0400

    i have a problem

--- web-library/src/main/java/ru/skypro/lessons/springboot/weblibrary/pojo/ReportRepository.java
+++ web-library/src/main/java/ru/skypro/lessons/springboot/weblibrary/pojo/ReportRepository.java
@@ -3,5 +3,4 @@ package ru.skypro.lessons.springboot.weblibrary.pojo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReportRepository extends JpaRepository<Report, Integer> {
-    Report findByEmployee(Employee employee);
 }
