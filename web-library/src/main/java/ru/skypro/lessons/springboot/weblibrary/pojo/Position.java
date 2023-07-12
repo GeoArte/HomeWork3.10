9e5bc07d6690bc12a7907c1ae598d6feb0a428f6
gosha <gosha.artemov@gmail.com>
2023-07-12 14:38:10 +0400

    i have a problem

---
+++ web-library/src/main/java/ru/skypro/lessons/springboot/weblibrary/pojo/Position.java
@@ -0,0 +1,31 @@
+package ru.skypro.lessons.springboot.weblibrary.pojo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "position")
+public class Position {
+
+    // Идентификатор должности, генерируется автоматически
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // Название должности
+    private String name;
+
+    public Position(){}
+
+    public Position(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
