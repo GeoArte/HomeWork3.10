f41928cc1d66a6d6bd8de3865611f37248aac8d6
gosha <gosha.artemov@gmail.com>
2023-09-01 15:41:56 +0400

    first commit

---
+++ web-library/src/test/java/ru/skypro/lessons/springboot/weblibrary/pojo/EmployeeServiceImplTest.java
@@ -0,0 +1,97 @@
+package ru.skypro.lessons.springboot.weblibrary.pojo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceImplTest {
+
+    @Test
+    void findAllEmployees() {
+    }
+
+
+    @Test
+    void getSalarySum() {
+    }
+
+    @Test
+    void getMinSalaryEmployee() {
+    }
+
+    @Test
+    void getMaxSalaryEmployee() {
+    }
+
+    @Test
+    void getHighSalaryEmployees() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployeeById() {
+    }
+
+    @Test
+    void getEmployeesWithSalaryGreaterThan() {
+    }
+
+    @Test
+    void getEmployeesWithHighestSalary() {
+    }
+
+    @Test
+    void getEmployeesByPosition() {
+    }
+
+    @Test
+    void getEmployeeFullInfo() {
+    }
+
+    @Test
+    void getEmployeesByPage() {
+    }
+
+    @Test
+    void getEmployeeReportById() {
+    }
+
+    @Test
+    void getDepartmentStatistics() {
+    }
+
+    @Test
+    void generateReportJson() {
+    }
+
+    @Test
+    void saveReportToFile() {
+    }
+
+    @Test
+    void saveReport() {
+    }
+
+    @Test
+    void readFileContent() {
+    }
+
+    @Test
+    void getReportFilePathById() {
+    }
+}
---
+++ web-library/src/test/java/ru/skypro/lessons/springboot/weblibrary/pojo/ReportServiceTest.java
@@ -0,0 +1,18 @@
+package ru.skypro.lessons.springboot.weblibrary.pojo;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ReportServiceTest {
+
+    @Test
+    void saveReport() {
+    }
+}
